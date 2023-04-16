0337dcaa5117c10e739c06acc7107ff64a9d4fba
YUJUNGCHEN <a0922335447@gmail.com>
2023-04-16 15:28:19 +0800

    feat: notify for booking_lunch & misson_management & weekend

--- notify.py
+++ notify.py
@@ -3,6 +3,14 @@ from datetime import date, datetime
 
 TODAY = date.today()
 
+NOW_HOUR = datetime.now().hour
+NOW_MINUTE = datetime.now().minute
+
+IS_EIGTH_THIRTY_AM = NOW_HOUR == 8 and NOW_MINUTE == 30
+IS_TEN_AM = NOW_HOUR == 10 and NOW_MINUTE == 0
+IS_ONE_THIRTY_PM = NOW_HOUR == 13 and NOW_MINUTE == 30
+IS_FIVE_THIRTY_PM = NOW_HOUR == 17 and NOW_MINUTE == 30
+
 MESSAGE_LIST = []
 
 
@@ -13,6 +21,8 @@ class MessageItem():
 
 
 def notify_meeting_day():
+    """行政小小會議
+    每個月的第二個禮拜四前兩天提醒"""
     meeting_list = [
         '2023-01-12',
         '2023-02-09',
@@ -41,6 +51,8 @@ def notify_meeting_day():
 
 
 def notify_write_weekly_report():
+    """工作週報
+    每週一早上跟下午提醒"""
     if TODAY.weekday() == 0:
         item = MessageItem()
         item.label = '➭ 工作週報'
@@ -48,7 +60,35 @@ def notify_write_weekly_report():
         MESSAGE_LIST.append(item)
 
 
-def format_message_list():
+def notify_wirte_misson_management():
+    """任務管理
+    每個月18跟20號提醒"""
+    if TODAY.day in [18, 20]:
+        item = MessageItem()
+        item.label = '➭ 任務管理'
+        item.content = '請需要撰寫任務管理的同仁於 20 號前完成，不要跟錢錢過意不去。'
+        MESSAGE_LIST.append(item)
+
+
+def notify_booking_lunch():
+    """午餐預約"""
+    item = MessageItem()
+    item.label = '➭ 訂午餐'
+    item.content = '十點嚕，記得訂午餐，有團購的話不要理我。'
+    MESSAGE_LIST.append(item)
+
+
+def notify_weekend():
+    """週末愉快"""
+    if TODAY.weekday() == 4:
+        item = MessageItem()
+        item.label = '➭ 週末愉快'
+        item.content = '今天是禮拜五，沒有人禮拜五加班的快回家，週末愉快。'
+        MESSAGE_LIST.append(item)
+
+
+def format_message_list() -> str:
+    """組合提醒訊息"""
     result_str = '\n\n '
 
     for (index, message_item) in enumerate(MESSAGE_LIST):
@@ -59,7 +99,8 @@ def format_message_list():
     return result_str
 
 
-def line_notify_message(token, msg):
+def line_notify_message(token: str, msg: str):
+    """發送 LINE 通知"""
     headers = {
         'Authorization': 'Bearer ' + token,
         'content-Type': 'application/x-www-form-urlencoded'
@@ -71,8 +112,16 @@ def line_notify_message(token, msg):
 
 
 if __name__ == '__main__':
-    notify_meeting_day()
-    notify_write_weekly_report()
+    if IS_EIGTH_THIRTY_AM:
+        notify_meeting_day()
+        notify_write_weekly_report()
+        notify_wirte_misson_management()
+    elif IS_TEN_AM:
+        notify_booking_lunch()
+    elif IS_ONE_THIRTY_PM:
+        notify_write_weekly_report()
+    elif IS_FIVE_THIRTY_PM:
+        notify_weekend()
 
     if MESSAGE_LIST:
         token = ''
